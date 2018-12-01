061ce78ebe108d249c6e4df5309e3542c88bfb7b
Maciej Jedwabny <maciej.jedwabny@intel.com>
2018-12-02 00:39:45 +0100

    Use mutable list to collect partial sums

--- day1/src/main/kotlin/io/github/mjedwabn/adventofcode2018/day1/part2.kt
+++ day1/src/main/kotlin/io/github/mjedwabn/adventofcode2018/day1/part2.kt
@@ -1,6 +1,8 @@
 package io.github.mjedwabn.adventofcode2018.day1
 
 import java.io.File
+import java.time.Duration
+import java.time.Instant
 
 fun main(args: Array<String>) {
     Day1Part2().run()
@@ -8,31 +10,34 @@ fun main(args: Array<String>) {
 
 class Day1Part2 {
     fun run() {
+        val start = Instant.now()
         val inputPath = javaClass.classLoader.getResource("input").path
         val frequencies = File(inputPath).readLines()
                 .map { f -> f.toInt() }
         val resultingFrequency = findFrequencyReachedTwice(frequencies)
         println(resultingFrequency)
+        val end = Instant.now()
+        println(Duration.between(start, end))
     }
 
     fun findFrequencyReachedTwice(frequencies: List<Int>): Int {
-        var sums: List<Int> = ArrayList()
+        val sums: MutableList<Int> = mutableListOf()
         var sum = 0
-        sums = sums.plus(sum)
+        sums.add(sum)
         var ret = 0
-        for (frequency in cycle(frequencies).iterator()) {
+        for (frequency in cycle(frequencies)) {
             sum += frequency
             if (sums.contains(sum)) {
                 ret = sum
                 break
             }
-            sums = sums.plus(sum)
+            sums.add(sum)
         }
         return ret
     }
 
-    private fun cycle(xs: List<Int>): Sequence<Int> {
+    private fun cycle(list: List<Int>): Sequence<Int> {
         var i = 0
-        return generateSequence { xs[i++ % xs.size] }
+        return generateSequence { list[i++ % list.size] }
     }
 }
--- day1/src/test/kotlin/io/github/mjedwabn/adventofcode2018/day1/Day1Part2Test.kt
+++ day1/src/test/kotlin/io/github/mjedwabn/adventofcode2018/day1/Day1Part2Test.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 internal class Day1Part2Test {
     @Test
-    internal fun name() {
+    internal fun sampleSequences() {
         assertEquals(0, Day1Part2().findFrequencyReachedTwice(listOf(1, -1)))
         assertEquals(10, Day1Part2().findFrequencyReachedTwice(listOf(3, 3, 4, -2, -4)))
         assertEquals(5, Day1Part2().findFrequencyReachedTwice(listOf(-6, 3, 8, 5, -6)))
